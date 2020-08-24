6139de363ce72b7d0793a5c8497fb66e8f67f511
Cool-Dude-x <69380703+Cool-Dude-x@users.noreply.github.com>
2020-08-25 01:16:08 +0530

    Update help.go

--- commands/help.go
+++ commands/help.go
@@ -1,27 +1,45 @@
-*  Copyright (C) 2020 by Anandpskerala@Github, < https://github.com/Anandpskerala >.
- *
- * This file is part of < https://github.com/Anandpskerala/ForwardTagRemoverBot > project,
- * and is released under the "GNU v3.0 License Agreement".
- * Please see < https://github.com/Anandpskerala/ForwardTagRemoverBot/blob/master/LICENSE >
- *
-oki
- */
 
-s
+
+
+package commands
 
 import (
-	"github.com/PaulSonOfLars/gotgbot"
-	"github.com/PaulSonOfLars/gotgbot/ext"
-	"github.com/PaulSonOfLars/gotgbot/parsemode"
-	"go.uber.org/zap"
+ "fmt"
+
+ "github.com/PaulSonOfLars/gotgbot"
+ "github.com/PaulSonOfLars/gotgbot/ext"
+ "github.com/PaulSonOfLars/gotgbot/parsemode"
+ "go.uber.org/zap"
 )
 
-func Help(b ext.Bot, u *gotgbot.Update) error {
+func Start(b ext.Bot, u *gotgbot.Update) error {
 
-	helpButton := [][]ext.InlineKeyboardButton{make([]ext.InlineKeyboardButton, 2), make([]ext.InlineKeyboardButton, 1)}
+ startButton := [][]ext.InlineKeyboardButton{make([]ext.InlineKeyboardButton, 2), make([]ext.InlineKeyboardButton, 1)}
 
-	helpButton[0][0] = ext.InlineKeyboardButton{
-		Text: "Source code",
-TagRemoverBot",
+ startButton[0][0] = ext.InlineKeyboardButton{
+  Text: "Join Group 1 ",
+  Url:  "https://t.me/missioncrackjeeneet",
+ }
 
+ startButton[0][1] = ext.InlineKeyboardButton{
+  Text: "Join Group 2 ",
+  Url:  "https://t.me/studymaterialmcjn",
+ }
 
+ startButton[1][0] = ext.InlineKeyboardButton{
+  Text: "My Owner",
+  Url:  "https://t.me/tausifur_123",
+ }
+
+ markup := ext.InlineKeyboardMarkup{InlineKeyboard: &startButton}
+
+ msg := b.NewSendableMessage(u.EffectiveChat.Id, fmt.Sprintf("Hi [%s](tg://user?id=%v)\nI am A Forward Tag remover Bot.Send /help To Know What I Can Do", u.EffectiveUser.FirstName, u.EffectiveUser.Id))
+ msg.ReplyToMessageId = u.EffectiveMessage.MessageId
+ msg.ReplyMarkup = &markup
+ msg.ParseMode = parsemode.Markdown
+ _, err := msg.Send()
+ if err != nil {
+  b.Logger.Warnw("Error in sending", zap.Error(err))
+ }
+ return err
+}
